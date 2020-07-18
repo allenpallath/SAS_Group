11a3704879a2b3ef1916cfcbc9b0b0dee005a70c
allenpallath <allenpallath@users.noreply.github.com>
2020-07-19 04:41:35 +0530

    commit

---
+++ js/custom.js
@@ -0,0 +1,81 @@
+(function ($) {
+
+  "use strict";
+
+    // PRE LOADER
+    $(window).load(function(){
+      $('.preloader').fadeOut(1000); // set duration in brackets    
+    });
+    
+
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).scroll(function() {
+      if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+          } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+          }
+    });
+    
+
+    // PARALLAX EFFECT
+    $.stellar({
+      horizontalScrolling: false,
+    }); 
+
+
+    // MAGNIFIC POPUP
+    $('.image-popup').magnificPopup({
+        type: 'image',
+        removalDelay: 300,
+        mainClass: 'mfp-with-zoom',
+        gallery:{
+          enabled:true
+        },
+        zoom: {
+        enabled: true, // By default it's false, so don't forget to enable it
+
+        duration: 300, // duration of the effect, in milliseconds
+        easing: 'ease-in-out', // CSS transition easing function
+
+        // The "opener" function should return the element from which popup will be zoomed in
+        // and to which popup will be scaled down
+        // By defailt it looks for an image tag:
+        opener: function(openerElement) {
+        // openerElement is the element on which popup was initialized, in this case its <a> tag
+        // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+        return openerElement.is('img') ? openerElement : openerElement.find('img');
+        }
+      }
+    });
+
+
+    // SMOOTH SCROLL
+    $(function() {
+      $('.custom-navbar a, #home a').on('click', function(event) {
+        var $anchor = $(this);
+          $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+          }, 1000);
+            event.preventDefault();
+      });
+    });  
+
+    document.getElementById("home").style.backgroundImage="url(../images/home1.jpg)";
+
+    var image = ["url(../images/home1.jpg)","url(../images/home2.jpg)","url(../images/home3.jpg)"];
+    var i = 0;
+    setInterval(function(){
+      if(i>3){
+        i = 0;
+      }
+      document.getElementById("home").style.backgroundImage=image[i];
+      i++;
+    },10000);
+
+    
+})(jQuery);
